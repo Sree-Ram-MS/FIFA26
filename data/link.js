32f0aaec9e6002a64f5b1bae0f4cc9524e127ad7
Sree Ram M S <srmsm3551@gmail.com>
2026-06-06 14:30:29 +0530

    [ADD] add links and countdown

--- data/link.js
+++ data/link.js
@@ -1,11 +1,25 @@
 // 🌐 Third-Party Streaming Websites
 window.THIRD_PARTY_SITES = [
+  { name: "90Live", link: "https://www.90live.in/" },
+  { name: "CazéTV", link: "https://www.youtube.com/cazetv" },
+  { name: "Easy Web TV", link: "https://zhangboheng.github.io/Easy-Web-TV-M3u8/routes/tv.html" },
   { name: "Epic Sports", link: "https://www.epicsports.in/" },
-  { name: "Live Sports", link: "https://livsport.pages.dev/" },
-  { name: "Sports Live", link: "https://livsport.dpdns.org/" },
+  { name: "Famelack", link: "https://famelack.com/" },
+  { name: "Footem", link: "https://www.footem.co.in/" },
+  { name: "Footy Feed", link: "https://footyfeed-4vg.pages.dev/"},
+  { name: "Hello Sports", link: "https://www.hellosports.live/" },
+  { name: "Koora Live", link: "https://kooralive-shoot.space/" },
   { name: "Live Score", link: "https://www.liverscore.net/" },
+  { name: "Live Sports", link: "https://livsport.pages.dev/" },
+  { name: "Rexdex Sports", link: "https://www.rexdexsports.in/" },
+  { name: "Sports Live", link: "https://livsport.dpdns.org/" },
   { name: "Sports Track", link: "https://www.sportstrack.in/" },
-  { name: "Sportsflair", link: "https://www.sportsflair.in/" }
+  { name: "Sports90", link: "https://www.sports90.in/" },
+  { name: "Sportsflair", link: "https://www.sportsflair.in/" },
+  { name: "StreamSports99", link: "https://streamsports99.fun/" },
+  { name: "Total Sports Live", link: "https://www.totalsportslive.co.zw/" },
+  { name: "Yalla Live", link: "https://yallalive.soccer/" },
+  { name: "Yalla Shoot", link: "https://www.yallashoot360.com/" },
 ];
 
 // 🔗 Direct Stream Links
